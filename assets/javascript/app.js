adda83b2f80040e47da945718a6c75af28d2b725
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-04-24 20:47:19 -0400

    initial set-up with links and basic styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyACRvX97cxEarRcZdnWENxquUL5u6TJlGw",
+    authDomain: "rock-paper-scissors-fb335.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-fb335.firebaseio.com",
+    projectId: "rock-paper-scissors-fb335",
+    storageBucket: "rock-paper-scissors-fb335.appspot.com",
+    messagingSenderId: "693363059042"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
