f20acd5d6e88af5618ebb018eb3828ab57c2b46c
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-04-26 22:49:15 -0400

    working on adding closed times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,6 @@ var config = {
 
 $("#add-train").on("click", function(event) {
     event.preventDefault();
-    console.log("you submitted!");
 
     // Grabbed values from text boxes
     name = $("#input-trainName").val().trim();
@@ -20,7 +19,6 @@ $("#add-train").on("click", function(event) {
     first = $("#input-firstTrain").val().trim();
     freq = $("#input-frequency").val().trim();
 
-        // TODO = add type control
     if (!name || !dest || !first || !freq) {
         alert("Please fill in all fields!");
         return;
@@ -31,28 +29,75 @@ $("#add-train").on("click", function(event) {
         dest: dest,
         first: first,
         freq: freq,
-        // min: min
     };
     database.ref().push({newTrain});
 
+    // reset input values
+    $("#input-trainName").val("");
+    $("#input-destination").val("");
+    $("#input-firstTrain").val("");
+    $("#input-frequency").val("");
 });
+var format = 'hh:mm'
+database.ref().on("child_added", function(snapshot) {
+    // Grab database values
+    var name = snapshot.val().newTrain.name;
+    var dest = snapshot.val().newTrain.dest;
+    var next = snapshot.val().newTrain.first;
+    var freq = snapshot.val().newTrain.freq;
+    // Format the database value for time
+    displayNext = moment(next, format);
+    // Difference between the times
+    var diffTime = moment().diff(moment(displayNext), "minutes");
 
-function makeRow(input) {
-    var row = $("<tr>");
-    var data = $("<td class='text-center'>");
-    $(data).text(input);
-    $(row).append(data);
-    // return row;
-    console.log(row);
+    // Time apart (remainder)
+    var tRemainder = diffTime % freq;
+
+
+    // is the station running?
+
+    var time = moment();
+    var beforeTime = moment('22:00', format);
+    var afterTime = moment('04:00', format);
+
+if (time.isBetween(beforeTime, afterTime)) {
+    // Minute Until Train
+    displayMinTo = freq - tRemainder;
+
+    // Next Train
+    displayNext = moment(moment().add(displayMinTo, "minutes")).format(format);
+    // create the new table row and add text
+    var newRow = $("<tr>").append(
+        
+        $("<td class='text-center'>").text(name), //name
+        $("<td class='text-center'>").text(dest), // destination
+        $("<td class='text-center'>").text(freq), // frequency
+        $("<td class='text-center'>").text(displayNext), // time of next arrival
+        $("<td class='text-center'>").text(displayMinTo) // minutes until time of next arrival
+      );
+
+} else {
+    // Time apart (remainder)
+    var tRemainder = diffTime % freq;
+    // Minute Until Train
+    displayMinTo = freq - tRemainder;
+
+    // Next Train
+    displayNext = moment(moment().add(displayMinTo, "minutes")).format(format);
+    // create the new table row and add text
+    var newRow = $("<tr>").append(
+        
+        $("<td class='text-center'>").text(name), //name
+        $("<td class='text-center'>").text(dest), // destination
+        $("<td class='text-center'>").text(freq), // frequency
+        $("<td class='text-center'>").text(snapshot.val().newTrain.first), // time of next arrival
+        $("<td class='text-center'>").text(displayMinTo) // minutes until time of next arrival
+      );
+
 }
 
-database.ref().on("value", function(snapshot) {
-
-    var displayName = makeRow(snapshot.val().name);
-    var displayDest = makeRow(snapshot.val().dest);
-    var displayFirst = makeRow(snapshot.val().first);
-    var displayFreq = makeRow(snapshot.val().freq);
-    var displayMin = makeRow(snapshot.val().min);
+        // add new table row to the html
+    $("#display-table > tbody").append(newRow);
 
     // Handle the errors
   }, function(errorObject) {
